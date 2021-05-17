c860f0569bfc2b4a36df48169d053c5b73e12d7b
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-17 15:31:44 +1000

    added html routes

---
+++ Develop/Routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = (app) => {
+    // directs user to notes page of app
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../public/notes.html'));
+
+    });
+    // directs user to home page of app
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../public/index.html'));
+    });
+};
